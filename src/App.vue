53203dcffbc50e5916faee81c095270b5a2ce009
Jesus <jlazar135@gmail.com>
2022-09-26 13:07:37 +0200

    se instala vue router

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,13 @@ import Ods from './components/Ods.vue'
 import Character from './components/Character.vue';
 import Resume from './components/Resume.vue';
 export default{
-  components: {
-    Form,
-    Ods,
-    Character,
-    Resume
-}
 }
 
 </script>
 
 <template>
   <div class="container">
-    <Form></Form>
-    <Ods></Ods>
-    <Character></Character>
-    <Resume></Resume>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 import { createApp, VueElement } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
+import Form from './components/Form.vue'
 
-createApp(App).mount('#app')
+// Componentes
+
+const Detail = {template: '<h2>Esto es una vista de detalles</h2>'}
+
+
+// definir objetos rutas 
+const routes = [
+    {
+        path: '/', component: Form
+    },
+    {
+        path: '/detalle', component: Detail
+    }
+]
+
+// crear objeto rutas vue router -- objeto VR
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+// instancia de Vue
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
